d705d785bf41cd15769a4e7cf79439f3a130fb69
Vinay Kumar Reddy Thadisina <W0850619@myscc.ca>
2024-02-21 20:39:29 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Illumination.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/Lamp.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Illumination.Models;
+
+public class Lamp
+{
+    public int Id { get; set; }
+    public string? LampType { get; set; }
+    public string? Genre { get; set; }
+    public decimal Price { get; set; }
+}
